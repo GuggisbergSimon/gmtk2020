eeeabfd0cd3ed3f46a27bbb5cdcaccd2771dc226
Ataor <slyguggi@gmail.com>
2020-07-11 00:17:22 +0200

    added GameManager and basic file hierarchy

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+    
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoadingScene;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoadingScene;
+    }
+
+    //this function is activated every time a scene is loaded
+    private void OnLevelFinishedLoadingScene(Scene scene, LoadSceneMode mode)
+    {
+        Setup();
+    }
+
+    private void Setup()
+    {
+        //alternative way to get elements. cons : if there is no element with such tag it creates an error
+        //_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        //_player = FindObjectOfType<PlayerController>();
+        //_uiManager = FindObjectOfType<UIManager>();
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        Setup();
+    }
+
+    public void LoadLevel(string nameLevel)
+    {
+        SceneManager.LoadScene(nameLevel);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+}
