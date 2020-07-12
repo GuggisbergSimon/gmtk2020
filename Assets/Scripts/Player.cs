b650883bc0cf5d14d22cf06f4cb01c392e4a8ec2
Stache <hugo.hueber@arcanite.ch>
2020-07-12 17:10:28 +0200

    I have no idea why I made

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -34,12 +34,14 @@ public class Player : MonoBehaviour
     private AudioSource _sourceRobot;
     private AudioSource _sourceItem;
     private PauseMenu _pauseMenu;
+    private WinPanel _winPanel;
 
     private void Start()
     {
         _levelManager = GameManager.Instance.LevelManager;
         _animator = GetComponent<Animator>();
-        _pauseMenu =  FindObjectOfType<PauseMenu>();
+        _pauseMenu = FindObjectOfType<PauseMenu>();
+        _winPanel = FindObjectOfType<WinPanel>();
         //audiosource setup
         _sourceRobot = gameObject.AddComponent<AudioSource>();
         _sourceRobot.playOnAwake = false;
@@ -195,7 +197,8 @@ public class Player : MonoBehaviour
             if (_levelManager.CheckFlags())
             {
                 Debug.Log("omedetou");
-                //todo win
+                // TODO
+                _winPanel.gameObject.SetActive(true);
             }
         }
 
