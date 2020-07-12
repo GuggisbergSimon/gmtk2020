57409573dd9eb7dfdcfe6c3045cbab1c513c8cf5
Stache <hugo.hueber@arcanite.ch>
2020-07-12 15:07:23 +0200

    Levels

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Tutorial : MonoBehaviour
+{
+    // Rebind de tes morts
+    public InputAction actionToBind;
+    public InputAction action;
+
+    private void Awake()
+    {
+        // KeyToAction()
+    }
+
+    private void KeyToAction(InputControl key, InputAction actionToBind)
+    {
+        GameManager.Instance.MappingCreator.AddAction(key, actionToBind);
+
+        //RIP decent programming practices
+        actionToBind.AddBinding(key);
+        //action.AddBinding(key);
+    }
+}
